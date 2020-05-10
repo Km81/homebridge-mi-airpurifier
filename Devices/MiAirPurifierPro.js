c47b7c6e298e8842f82437c4a631e4d45ad2ff58
Km81 <km81.ryu@icloud.com>
2020-05-10 11:34:05 +0900

    Update MiAirPurifierPro.js

--- Devices/MiAirPurifierPro.js
+++ Devices/MiAirPurifierPro.js
@@ -76,13 +76,8 @@ MiAirPurifierProAirPurifierAccessory.prototype.getServices = function() {
     var activeCharacteristic = airPurifierService.getCharacteristic(Characteristic.Active);
     var currentAirPurifierStateCharacteristic = airPurifierService.getCharacteristic(Characteristic.CurrentAirPurifierState);
     var targetAirPurifierStateCharacteristic = airPurifierService.getCharacteristic(Characteristic.TargetAirPurifierState);
-    var lockPhysicalControlsCharacteristic = airPurifierService.addCharacteristic(Characteristic.LockPhysicalControls);
     var rotationSpeedCharacteristic = airPurifierService.addCharacteristic(Characteristic.RotationSpeed);
-    
-    var currentTemperatureCharacteristic = airPurifierService.addCharacteristic(Characteristic.CurrentTemperature);
-	var currentRelativeHumidityCharacteristic = airPurifierService.addCharacteristic(Characteristic.CurrentRelativeHumidity);
-    var pm25DensityCharacteristic = airPurifierService.addCharacteristic(Characteristic.PM2_5Density);
-    var airQualityCharacteristic = airPurifierService.addCharacteristic(Characteristic.AirQuality);
+
     services.push(airPurifierService);
     
     silentModeOnCharacteristic
@@ -201,30 +196,6 @@ MiAirPurifierProAirPurifierAccessory.prototype.getServices = function() {
                 callback(err);
             });
         }.bind(this));
-
-    lockPhysicalControlsCharacteristic
-        .on('get', function(callback) {
-            that.device.call("get_prop", ["child_lock"]).then(result => {
-                that.platform.log.debug("[MiAirPurifierPlatform][DEBUG]MiAirPurifierProAirPurifierAccessory - LockPhysicalControls - getLockPhysicalControls: " + result);
-                callback(null, result[0] === "on" ? Characteristic.LockPhysicalControls.CONTROL_LOCK_ENABLED : Characteristic.LockPhysicalControls.CONTROL_LOCK_DISABLED);
-            }).catch(function(err) {
-                that.platform.log.error("[MiAirPurifierPlatform][ERROR]MiAirPurifierProAirPurifierAccessory - LockPhysicalControls - getLockPhysicalControls Error: " + err);
-                callback(err);
-            });
-        }.bind(this))
-        .on('set', function(value, callback) {
-            that.device.call("set_child_lock", [value ? "on" : "off"]).then(result => {
-                that.platform.log.debug("[MiAirPurifierPlatform][DEBUG]MiAirPurifierProAirPurifierAccessory - LockPhysicalControls - setLockPhysicalControls Result: " + result);
-                if(result[0] === "ok") {
-                    callback(null);
-                } else {
-                    callback(new Error(result[0]));
-                }
-            }).catch(function(err) {
-                that.platform.log.error("[MiAirPurifierPlatform][ERROR]MiAirPurifierProAirPurifierAccessory - LockPhysicalControls - setLockPhysicalControls Error: " + err);
-                callback(err);
-            });
-        }.bind(this));
         
     targetAirPurifierStateCharacteristic
         .on('get', function(callback) {
@@ -312,54 +283,6 @@ MiAirPurifierProAirPurifierAccessory.prototype.getServices = function() {
                 })
             }
         }.bind(this));
-
-    currentTemperatureCharacteristic.on('get', function(callback) {
-        this.device.call("get_prop", ["temp_dec"]).then(result => {
-            that.platform.log.debug("[MiAirPurifierPlatform][DEBUG]MiAirPurifier2AirPurifierAccessory - Temperature - getTemperature: " + result);
-            callback(null, result[0] / 10);
-        }).catch(function(err) {
-            that.platform.log.error("[MiAirPurifierPlatform][ERROR]MiAirPurifier2AirPurifierAccessory - Temperature - getTemperature Error: " + err);
-            callback(err);
-        });
-    }.bind(this));
-
-    currentRelativeHumidityCharacteristic
-	    .on('get', function(callback) {
-			this.device.call("get_prop", ["humidity"]).then(result => {
-                that.platform.log.debug("[MiAirPurifierPlatform][DEBUG]MiAirPurifier2AirPurifierAccessory - Humidity - getHumidity: " + result);
-                callback(null, result[0]);
-            }).catch(function(err) {
-                that.platform.log.error("[MiAirPurifierPlatform][ERROR]MiAirPurifier2AirPurifierAccessory - Humidity - getHumidity Error: " + err);
-                callback(err);
-            });
-	    }.bind(this));
-
-    pm25DensityCharacteristic
-	    .on('get', function(callback) {
-			this.device.call("get_prop", ["aqi"]).then(result => {
-                that.platform.log.debug("[MiAirPurifierPlatform][DEBUG]MiAirPurifier2AirPurifierAccessory - aqi - getHumidity: " + result);
-                callback(null, result[0]);
-                
-                var airQualityValue = Characteristic.AirQuality.UNKNOWN;
-                if(result[0] <= 5) {
-                    airQualityValue = Characteristic.AirQuality.EXCELLENT;
-                } else if(result[0] > 5 && result[0] <= 15) {
-                    airQualityValue = Characteristic.AirQuality.GOOD;
-                } else if(result[0] > 15 && result[0] <= 35) {
-                    airQualityValue = Characteristic.AirQuality.FAIR;
-                } else if(result[0] > 35 && result[0] <= 55) {
-                    airQualityValue = Characteristic.AirQuality.INFERIOR;
-                } else if(result[0] > 55) {
-                    airQualityValue = Characteristic.AirQuality.POOR;
-                } else {
-                    airQualityValue = Characteristic.AirQuality.UNKNOWN;
-                }
-                airQualityCharacteristic.updateValue(airQualityValue);
-            }).catch(function(err) {
-                that.platform.log.error("[MiAirPurifierPlatform][ERROR]MiAirPurifier2AirPurifierAccessory - aqi - getHumidity Error: " + err);
-                callback(err);
-            });
-	    }.bind(this));
 
     // var filterMaintenanceService = new Service.FilterMaintenance(this.name);
     var filterChangeIndicationCharacteristic = airPurifierService.getCharacteristic(Characteristic.FilterChangeIndication);
